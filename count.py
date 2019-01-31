2ac9fce8a00c8e980141c13a1f03cb76fe316ce7
Francis Tseng <f+accounts@frnsys.com>
2019-01-31 16:34:20 -0500

    init

---
+++ count.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from collections import defaultdict
+
+# Load the exported Google Form CSV
+df = pd.read_csv('votes.csv')
+
+# Extract only the ranking columns
+vote_cols = df.columns[2:]
+df = df[vote_cols]
+
+# Get how many ranks were available to give
+n_ranks = len(vote_cols)
+
+scores = defaultdict(int)
+
+# Assuming first column is highest rank
+for i, col in enumerate(df.columns):
+    points = n_ranks - i
+    for candidate in df[col]:
+        scores[candidate] += points
+
+ranking = sorted(scores.items(), key=lambda i: i[1], reverse=True)
+for cand, score in ranking:
+    print(cand, score)
---
+++ readme.md
@@ -0,0 +1,5 @@
+Borda count voting,
+implemented based on:
+<https://en.wikipedia.org/wiki/Borda_count#Borda's_system_(starting_at_1)>
+
+This assumes a CSV file called `votes.csv` where the rankings start in the third column (column index 2).
