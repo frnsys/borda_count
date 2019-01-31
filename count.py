86b37d14b5906601c00e5f5e5b48896054497487
Francis Tseng <f+accounts@frnsys.com>
2019-01-31 16:43:32 -0500

    show score if base points are based off number of candidates and if they are based on number of ranks

--- count.py
+++ count.py
@@ -8,17 +8,31 @@ df = pd.read_csv('votes.csv')
 vote_cols = df.columns[2:]
 df = df[vote_cols]
 
+# Calculate total number of candidates
+candidates = set()
+for i, col in enumerate(df.columns):
+    for candidate in df[col]:
+        candidates.add(candidate)
+n_candidates = len(candidates)
+print(n_candidates, 'candidates')
+
 # Get how many ranks were available to give
 n_ranks = len(vote_cols)
+print(n_ranks, 'ranks')
 
-# Assuming first column is highest rank
-scores = defaultdict(int)
-for i, col in enumerate(df.columns):
-    points = n_ranks - i
-    for candidate in df[col]:
-        scores[candidate] += points
+print('='*20)
 
-# Sort candidates by score, descending
-ranking = sorted(scores.items(), key=lambda i: i[1], reverse=True)
-for cand, score in ranking:
-    print(cand, score)
+for label, base_points in [('candidates', n_candidates), ('ranks', n_ranks)]:
+    print('Using {} as base points:'.format(label))
+    # Assuming first column is highest rank
+    scores = defaultdict(int)
+    for i, col in enumerate(df.columns):
+        points = base_points - i
+        for candidate in df[col]:
+            scores[candidate] += points
+
+    # Sort candidates by score, descending
+    ranking = sorted(scores.items(), key=lambda i: i[1], reverse=True)
+    for cand, score in ranking:
+        print(cand, score)
+    print('='*20)
